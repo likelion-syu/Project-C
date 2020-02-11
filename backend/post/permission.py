684a1a5166561c46063bbe1cae420fa50cc530ba
sneakstarberry <gihong2012@likelion.org>
2020-02-11 17:10:10 +0900

    <permission 오류 수정> - true, True 변경

--- backend/post/permission.py
+++ backend/post/permission.py
@@ -3,8 +3,8 @@ from rest_framework import permissions
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
-            return true
+            return True
         if request.method is 'POST':
-            return true
+            return True
 
         return obj.author == request.user
